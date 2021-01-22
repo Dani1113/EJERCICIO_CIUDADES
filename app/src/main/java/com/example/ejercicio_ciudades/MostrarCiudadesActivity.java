a91eeed5522d9a6ec65bfe729478d7e7c8829152
dani <danirm1113@gmail.com>
2021-01-22 17:54:21 +0100

    Creación del RecyclerView

---
+++ app/src/main/java/com/example/ejercicio_ciudades/MostrarCiudadesActivity.java
@@ -0,0 +1,14 @@
+package com.example.ejercicio_ciudades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarCiudadesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_ciudades);
+    }
+}
